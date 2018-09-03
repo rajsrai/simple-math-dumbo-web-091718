f5f5ff1c4a72f063b525bb5822ca5ad4f2a28060
Rajvir Rai <rsr1992@gmail.com>
2018-09-03 16:54:01 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,19 +7,19 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  10/2
+  50/2
 end
 
 def multiplication(num1, num2)
-  10 * 10
+  4 * 30
 end
 
 def modulo(num1, num2)
-  10 % 5 
+  34 % 5
 end
 
 def square_root(num)
-  Math.sqrt(10)
+  Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
