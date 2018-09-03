ea78c72981c9c5dbb964ed46f0dcb15a47c95ea7
Rajvir Rai <rsr1992@gmail.com>
2018-09-03 16:52:04 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  1 + 1
 end
 
 def subtraction(num1, num2)
+  10 - 5
 end
 
 def division(num1, num2)
+  10/2
 end
 
 def multiplication(num1, num2)
+  10 * 10
 end
 
 def modulo(num1, num2)
+  10 % 5 
 end
 
 def square_root(num)
+  Math.sqrt(10)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  5 + 2 - (7 - 2) + 1
 end
